29d4ad3dcd109f363b8c83f75446c13645a15522
Miguel Lorenzo <jsmglorenzo@gmail.com>
2020-06-19 13:44:01 -0400

    changed config db url

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://localhost/saf-db',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://jsmglorenzo@localhost/saf-db',
     ACC_SID:'AC395431c63b547c8296902e1becddb56a',
     AUTH_TOKEN:'4908b0756f4bbb3ad204f277e0956433',
     FROM: '+12029463255',
