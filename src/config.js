71bfcf6aafedd0ad5f02dbc2a22afd338a39ef4e
Miguel Lorenzo <jsmglorenzo@gmail.com>
2020-06-20 18:37:30 -0400

    client origin test

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://jsmglorenzo@localhost/saf-db',
-    CLIENT_ORIGIN: 'https://saf-textapp-client.jml0123.vercel.app/',
+    CLIENT_ORIGIN: 'http://localhost:3000',
 }
 // Add to env file and hide credentials
